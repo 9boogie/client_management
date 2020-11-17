c24fb6674d645a2ebc1eb1331dc2378a22d6ede8
9boogie <jzero.youngs89@gmail.com>
2020-11-17 01:36:39 -0600

    Create database and connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,25 @@ const port = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
 app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      id: 1,
-      image: 'https://placeimg.com/64/64/1',
-      name: 'Harry',
-      birthday: '961222',
-      gender: 'Male',
-      job: 'Laywer'
-    },
-    {
-      id: 2,
-      image: 'https://placeimg.com/64/64/2',
-      name: 'Ron',
-      birthday: '940129',
-      gender: 'Male',
-      job: 'Doctor'
-    },
-    {
-      id: 3,
-      image: 'https://placeimg.com/64/64/3',
-      name: 'Miz',
-      birthday: '980722',
-      gender: 'Female',
-      job: 'student'
+  connection.query(
+    'SELECT * FROM customer;',
+    (err, rows, fields) => {
+      res.send(rows);
     }
-  ])
+  )
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
